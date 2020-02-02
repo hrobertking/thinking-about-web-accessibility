f8a9cce32ce56ca97cb96623ab45f63c7d211df4
Robert King <hrobertking@cathmhaol.com>
2020-02-02 14:13:56 -0700

    fix(table): inject button

--- table.js
+++ table.js
@@ -3,19 +3,19 @@
   var description = document.getElementById(
       table.getAttribute('aria-describedby')
     ) || document.createElement('div'),
-    sort = function (e) {
-      var target = e.target,
+    sort = function (th) {
+      var th,
         ordering = {
           ascending: 1,
           descending: -1,
           none: 0
         },
         transition = ['none', 'ascending', 'descending'],
-        currentSort = target.getAttribute('aria-sort'),
+        currentSort = th.getAttribute('aria-sort'),
         nextSort = transition.indexOf(currentSort) + 1,
         sortOrder = transition[nextSort] || transition[0],
         order = ordering[sortOrder],
-        col = target ? target.cellIndex : null,
+        col = th ? th.cellIndex : null,
         body = table.tBodies.item(0),
         tbody = document.createElement('tbody'),
         sortAfter = 1 * order,
@@ -59,31 +59,45 @@
         tbody.appendChild(row);
       });
       table.replaceChild(tbody, body);
-      target.setAttribute('aria-sort', sortOrder);
-      description.text = 'Sorted by ' +
-        target.innerText + ': ' + sortOrder;
+      th.setAttribute('aria-sort', sortOrder);
+      description.content = 'Sorted by ' +
+        th.innerText + ': ' + sortOrder;
     };
-  Object.defineProperty(description, 'text', {
-    set: function (message) {
-      function removeClass() {
-        description.className = (description.className || '')
-          .replace(/\bupdated\b/, ' ').trim();
+  if (!description.hasOwnProperty('content')) {
+    Object.defineProperty(description, 'content', {
+	  enumerable: true,
+      get: function () {
+        return this.innerText;
+      },
+      set: function (message) {
+        function removeClass() {
+          description.className = (description.className || '')
+            .replace(/\bupdated\b/, ' ').trim();
+        }
+
+        this.innerHTML = message;
+        this.className = [
+          this.className,
+          'updated'
+        ].join(' ').trim();
+
+        window.setTimeout(removeClass, 3000);
       }
-
-      this.innerHTML = message;
-      this.className = [
-        this.className,
-        'updated'
-      ].join(' ').trim();
-
-      window.setTimeout(removeClass, 3000);
-    }
-  });
-
+    });
+  }
+  
   [].slice.call(table.getElementsByTagName('th'))
   .forEach(function (th) {
     if (th.getAttribute(`aria-sort`) !== '') {
-      th.addEventListener('click', sort);
+	  var btn = document.createElement('button'),
+		handler = function () {
+		  sort(th);
+		}
+	  btn.type = 'button';
+	  btn.innerHTML = th.innerHTML;
+      btn.addEventListener('click', handler);
+	  th.innerHTML = '';
+	  th.appendChild(btn);
     }
   })
 });
