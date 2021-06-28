9100366fadc9da9fad4c2f95056900b84affa668
Robert King <hrobertking@yahoo.com>
2021-06-28 14:20:11 -0700

    Create input.js

---
+++ input.js
@@ -0,0 +1,12 @@
+[].slice.call(document.getElementsByTagName('input'))
+.concat([].slice.call(document.getElementsByTagName('textarea')))
+.forEach(function TextInput(input) {
+  Object.defineProperty(input, 'characterCount', {
+    enumerable: true,
+    get: function () { return this.value.length; }
+  });
+  Object.defineProperty(input, 'wordCount', {
+    enumerable: true,
+    get: function () { return this.value.split(' ').length; }
+  });
+});
